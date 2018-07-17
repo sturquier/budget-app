90a9d63da52218707eba7777bdc9595d85b03025
Simon Turquier <turquiersimon@hotmail.fr>
2018-07-17 13:04:01 +0200

    Add dependencies & setup routing

--- src/constants/Routes.js
+++ src/constants/Routes.js
@@ -1,3 +1,4 @@
+export const ROOT_PAGE 					= '/'
 export const EXPENSES_DASHBOARD_PAGE 	= '/expenses'
 export const ADD_EXPENSE_PAGE 			= '/expenses/new'
 export const EDIT_EXPENSE_PAGE			= '/expenses/:id'
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import { HashRouter, Switch, Route, Redirect } from 'react-router-dom'
+import * as routes from '../constants/Routes'
+import ExpensesDashboard from '../components/expenses/ExpensesDashboard'
+import AddExpense from './expenses/AddExpense'
+import EditExpense from './expenses/EditExpense'
 
 class App extends Component {
-	
+
   	render() {
 		return (
-	  		<div>To get started, edit <code>src/App.js</code> and save to reload.</div>
+	  		<div>
+	  			<HashRouter>
+	  				<Switch>
+	  					<Route exact path={routes.ROOT_PAGE} render={() => (
+	  						<Redirect to={routes.EXPENSES_DASHBOARD_PAGE}/>
+	  					)}/>
+	  					<Route exact path={routes.EXPENSES_DASHBOARD_PAGE} component={ExpensesDashboard}/>
+	  					<Route exact path={routes.ADD_EXPENSE_PAGE} component={AddExpense}/>
+	  					<Route exact path={routes.EDIT_EXPENSE_PAGE} component={EditExpense}/>
+	  				</Switch>
+	  			</HashRouter>
+	  		</div>
 		)
   	}
 }

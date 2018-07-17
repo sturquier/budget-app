6b8e6f4b78186cd2d46e6e604c94f2877518ce1c
Simon Turquier <turquiersimon@hotmail.fr>
2018-07-17 16:46:22 +0200

    Add EditExpense form

--- src/containers/expenses/EditExpense.js
+++ src/containers/expenses/EditExpense.js
@@ -1,12 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { editExpense, removeExpense } from '../../actions/expenses'
+import ExpenseForm from '../../components/expenses/ExpenseForm'
+import { EXPENSES_DASHBOARD_PAGE } from '../../constants/Routes'
 
-const EditExpense = () => {
+class EditExpense extends Component {
 
-	return (
-		<div>
-			<h4>Edit Expense</h4>
-		</div>
-	)
+	constructor(props) {
+		super(props)
+		this.removeExpense = this.removeExpense.bind(this)
+	}
+
+	render() {
+		return (
+			<div>
+				<h4>Edit Expense</h4>
+				<hr/>
+				<button className="btn btn-danger" onClick={this.removeExpense}>Remove</button>
+				<br/>
+				<br/>
+				<ExpenseForm
+					expense={this.props.expense}
+					submitExpenseCallback={(expense) => this.submitExpenseCallback(expense)}
+				/>
+			</div>
+		)
+	}
+
+	submitExpenseCallback(expense) {
+		this.props.editExpense(this.props.match.params.id, expense)
+		this.props.history.push(EXPENSES_DASHBOARD_PAGE)
+	}
+
+	removeExpense() {
+		this.props.removeExpense(this.props.match.params.id)
+		this.props.history.push(EXPENSES_DASHBOARD_PAGE)
+	}
 }
 
-export default EditExpense
+const mapStateToProps = (state, props) => {
+	return {
+		expense: state.expenses.find((expense) => expense.id === props.match.params.id)
+	}
+}
+
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({editExpense, removeExpense}, dispatch)
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(EditExpense)

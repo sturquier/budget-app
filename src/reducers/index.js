0cc4096756baefc850b9ab9bc79092c95b850ea9
Simon Turquier <turquiersimon@hotmail.fr>
2018-07-17 17:26:10 +0200

    Add filters reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 import ExpensesReducer from './expenses'
+import FiltersReducer from './filters'
 import { reducer as FormReducer } from 'redux-form'
 
 const rootReducer = combineReducers({
 	expenses: ExpensesReducer,
+	filters: FiltersReducer,
 	form: FormReducer
 })
 

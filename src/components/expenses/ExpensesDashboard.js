3288dde952be8ae609cc927d667b3f5a271c3349
Simon Turquier <turquiersimon@hotmail.fr>
2018-07-19 12:13:24 +0200

    Fix authorization for dashboard

--- src/components/expenses/ExpensesDashboard.js
+++ src/components/expenses/ExpensesDashboard.js
@@ -1,23 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
+import { connect } from 'react-redux'
+import { compose } from 'recompose'
 
 import ExpensesSummary from '../../containers/expenses/ExpensesSummary'
 import ExpensesFilters from '../../containers/expenses/ExpensesFilters' 
 import ExpensesList from '../../containers/expenses/ExpensesList'
-import withAuthorization from '../session/withAuthorization' 
+import withAuthorization from '../session/withAuthorization'
 
-class ExpensesDashboard extends Component {
+const ExpensesDashboard = ({ authUser }) => {
 
-	render() {
-		return (
-			<div>
-				<ExpensesSummary/>
-				<ExpensesFilters/>
-				<ExpensesList/>
-			</div>
-		)
+	return (
+		<div>
+			<ExpensesSummary/>
+			<ExpensesFilters/>
+			<ExpensesList/>
+		</div>
+	)
+}
+
+const mapStateToProps = (state) => {
+	return {
+		authUser: state.session.authUser
 	}
 }
 
-const authCondition = (authUser) => !!authUser
+const authCondition = (authUser) => !! authUser
 
-export default withAuthorization(authCondition)(ExpensesDashboard)
+export default compose(
+	withAuthorization(authCondition),
+	connect(mapStateToProps),
+)(ExpensesDashboard)
